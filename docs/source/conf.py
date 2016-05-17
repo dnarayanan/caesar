5524342d5fc51de0aec8e848ff103c1473bd06ad
Robert Thompson <rthompsonj@gmail.com>
2016-05-16 20:05:39 -0500

    adding rtd conditional in the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -112,9 +112,15 @@ todo_include_todos = False
 
 # -- Options for HTML output ----------------------------------------------
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+_html_theme = 'alabaster'
+if on_rtd:
+    _html_theme = 'default'
+html_theme = _html_theme
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
