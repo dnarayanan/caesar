91ecf8a32635b0ba3232e3051c4cf6b9a9fdfa77
Robert Thompson <rthompsonj@gmail.com>
2016-05-18 16:49:28 -0500

    just kidding, RTD still doesn't actually read the binary!

---
+++ caesar/group_funcs/group_funcs.py
@@ -0,0 +1,115 @@
+## SHELL FILES FOR DOCUMENTATION PURPOSES           ##
+## if you want to see the source please examine     ##
+## caesar/group_funcs/group_funcs.pyx               ##
+
+import numpy as np
+
+def get_periodic_r(
+        double boxsize,
+        np.ndarray[np.float64_t, ndim=1] center,
+        np.ndarray[np.float64_t, ndim=2] pos,
+        np.ndarray[np.float64_t, ndim=1] r
+):
+    """Get periodic radii.
+
+    Parameters
+    ----------
+    boxsize : double
+        The size of your domain.
+    center : np.ndarray([x,y,z])
+        Position in which to calculate the radius from.
+    pos : np.ndarray
+        Nx3 numpy array containing the positions of particles.
+    r : np.array
+        Empty array to fill with radius values.
+
+    """
+    pass
+
+
+def rotator(
+        np.ndarray[np.float64_t, ndim=2] vals,
+        np.ndarray[np.float64_t, ndim=2] Rx,
+        np.ndarray[np.float64_t, ndim=2] Ry,        
+        double ALPHA, double BETA
+):
+    """Rotate a number of vectors around ALPHA, BETA
+
+    Parameters
+    ----------
+    vals : np.ndarray
+        Nx3 np.ndarray of values you want to rotate.
+    Rx : np.ndarray 
+        3x3 array used for the first rotation about ALPHA.
+        The dot product is taken against each value:
+        vals[i] = np.dot(Rx, vals[i])
+    Ry : np.ndarray
+        3x3 array used for the second rotation about BETA
+        The dot product is taken against each value:
+        vals[i] = np.dot(Ry, vals[i])
+    ALPHA : double
+        Angle to rotate around first.
+    BETA : double
+        Angle to rotate around second.
+
+    Notes
+    -----
+    This is typically called from :func:`utils.rotator`.
+
+    """
+    pass
+
+
+def get_half_mass_radius(
+        np.ndarray[np.float64_t, ndim=1] mass,
+        np.ndarray[np.float64_t, ndim=1] radii,
+        np.ndarray[np.int32_t, ndim=1] ptype,
+        double half_mass,
+        int binary
+):
+    """Get half mass radius for a set of particles.
+
+    Parameters
+    ----------
+    mass : np.ndarray
+        Masses of particles.
+    radii : np.ndarray
+        Radii of particles.
+    ptype : np.ndarray
+        Array of integers containing the particle types.
+    half_mass : double
+        Half mass value to accumulate to.
+    binary : int
+        Integer used to select particle types.  For example,
+        if you are interested in particle types 0 and 3 this
+        value would be 2^0+2^3=9.
+
+    """
+    pass
+
+def get_full_mass_radius(
+        np.ndarray[np.float64_t, ndim=1] radii,
+        np.ndarray[np.int32_t, ndim=1] ptype,
+        int binary
+):
+    """Get full mass radius for a set of particles.
+
+    Parameters
+    ----------
+    radii : np.ndarray[::-1]
+        Radii of particles
+    ptype : np.ndarray[::-1]
+        Array of integers containing the particle types.
+    binary : int
+        Integer used to select particle types.  For example,
+        if you are interested in particle types 0 and 3 this
+        value would be 2^0+2^3=9.
+
+    Notes
+    -----
+    This function iterates forward through the array, so it
+    is advisable to reverse the radii & ptype arrays before 
+    passing them via np.ndarray[::-1].
+
+    """
+    pass
---
+++ caesar/hydrogen_mass_calc/hydrogen_mass_calc.py
@@ -0,0 +1,79 @@
+## SHELL FILES FOR DOCUMENTATION PURPOSES           ##
+## if you want to see the source please examine     ##
+## caesar/hydrogen_mass_calc/hydrogen_mass_calc.pyx ##
+
+import numpy as np
+
+def check_values(obj):
+    """Check to make sure that we have the required fields available to
+    perform the hydrogen mass frac calculation.
+
+    Parameters
+    ----------
+    obj : :class:`main.CAESAR`
+        Main CAESAR object.
+    
+    Returns
+    -------
+    bool
+        Returns True if all fields are present, False otherwise.
+
+    """
+    pass
+
+
+def hydrogen_mass_calc(obj,**kwargs):
+    """Calculate the neutral and molecular mass contents of SPH particles.
+    
+    For non star forming gas particles assigned to halos we calculate
+    the neutral fraction based on equations from Popping+09 and
+    Rahmati+13.  If H2 block is not present in the simulation file we
+    estimate the neutral and molecular fraciton via Leroy+08.  Once
+    these fractions are calculated we assign HI/H2 masses to galaxies
+    & halos based on their mass-weighted distances.
+
+    Parameters
+    ----------
+    obj : :class:`main.CAESAR`
+        Main CAESAR object.
+
+    Returns
+    -------
+    HImass, H2mass : np.ndarray, np.ndarray
+        Contains the HImass and H2 mass of each individual particle.
+
+    """
+    pass
+
+
+def assign_halo_gas_to_galaxies(
+        ## internal to the halo
+        np.ndarray[np.float64_t,ndim=2]   internal_galaxy_pos,
+        np.ndarray[np.float64_t,ndim=1]   internal_galaxy_mass,
+        np.ndarray[np.int32_t,ndim=1]     internal_glist,
+        np.ndarray[np.int32_t,ndim=1]     internal_galaxy_index_list,
+        ## global values
+        np.int32_t[:]     galaxy_glist,
+        np.float64_t[:]   grhoH,
+        np.float64_t[:,:] gpos,
+        np.float64_t[:]   galaxy_HImass,
+        np.float64_t[:]   galaxy_H2mass,
+        np.float64_t[:]   HImass,
+        np.float64_t[:]   H2mass,
+        double low_rho_thresh,
+        double boxsize,
+        double halfbox
+):    
+    """Function to assign halo gas to galaxies.
+
+    When we assign galaxies in CAESAR, we only consider dense gas.
+    But when considering HI gas however, it is often desirable to
+    also consider low-density gas 'outside' of the galaxy.  This
+    function calculates the mass weighted distance to each galaxy
+    within a given halo and assigns low-density gas to the 'nearest'
+    galaxy.
+
+    Typically called from :func:`hydrogen_mass_calc.hydrogen_mass_calc`.
+
+    """
+    pass
