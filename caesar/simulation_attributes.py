Fix self-shielding options; fix virial_density computation; hopefully fix installation issue with get_virial_mr

caesar/simulation_attributes.py
@@ -1,4 +1,5 @@
 import numpy as np
+from yt.funcs import mylog
 
 class SimulationAttributes(object):
     """A class to hold simulation attributes."""
@@ -46,6 +47,9 @@ class SimulationAttributes(object):
             )
             self.Om_z = Om_0 * (1.0 + self.redshift)**3 / self.E_z**2
             H_z       = H0 * self.E_z
+            #Kitayama & Suto 1996 v.469, p.480
+            fomega = Om_0*(1.+self.redshift)**3 / ( Om_0*(1.+self.redshift)**3 + (1.-Om_0-Ol_0)*(1.+self.redshift)**2 + Ol_0)
+            if fomega >=1.0: mylog.warning('fomega out of bounds! fomega=%g %g %g %g'%(fomega,self.Om_z,self.redshift,Ol_0))
         else:
             H_z = H0
 
@@ -53,6 +57,7 @@ class SimulationAttributes(object):
                 self.Om_z = ds.cosmology.omega_matter
             else:
                 self.Om_z = 0.3
+            fomega = self.Om_z
 
                 
         self.H_z = ds.quan(H_z * 3.24077929e-20, '1/s')
@@ -62,9 +67,7 @@ class SimulationAttributes(object):
             (3.0 * self.H_z.d**2) / (8.0 * np.pi * self.G.d),
             'Msun / kpc**3'
         )
-        #Kitayama & Suto 1996 v.469, p.480
-        fomega = self.Om_z*(1.+self.redshift)**3 / ( self.Om_z*(1.+self.redshift)**3 + (1.-self.Om_z-Ol_0)*(1.+self.redshift)**2 + Ol_0)
-        virial_density = (177.65287921960845*(1. + 0.4093*(1./fomega - 1.)**0.9052) - 1.)*self.Om_z#18*pi*pi
+        virial_density = (177.65287921960845*(1. + 0.4093*(1./fomega - 1.)**0.9052) - 1.)*self.Om_z #Kitayama & Suto 1996 v.469, p.480
         self.Densities = np.array([virial_density*self.critical_density.to('Msun/kpc**3').d,
                                              200.*self.critical_density.to('Msun/kpc**3').d,
                                              500.*self.critical_density.to('Msun/kpc**3').d,