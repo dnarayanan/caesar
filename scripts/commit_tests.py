8da7fdf179c885c96fa6faecd2847a224aeb3d48
Ben Kimock <kimockb@gmail.com>
2020-05-02 17:24:47 -0400

    clean up testing script, actually run FoF in action

--- scripts/commit_tests.py
+++ scripts/commit_tests.py
@@ -1,49 +1,31 @@
-import caesar
-import yt
+import os
 import numpy as np
+import yt
+import caesar
 
 #MODIFIABLE HEADER POINTING TO LOCATION OF TESTING SNAPSHOTS
-simba_snap = '/ufrc/narayanan/desika.narayanan/caesar_testing_files/m25n256_full_passive/snapshot_305.hdf5'
+snaps = [
+    '/ufrc/narayanan/desika.narayanan/caesar_testing_files/m25n256_full_passive/snapshot_305.hdf5',
+    '/ufrc/narayanan/desika.narayanan/caesar_testing_files/yt_gizmo_64/output/snap_N64L16_135.hdf5',
+]
 
-small_snap = '/ufrc/narayanan/desika.narayanan/caesar_testing_files/yt_gizmo_64/output/snap_N64L16_135.hdf5'
+# Also attempt to caesar everything that we're passed as CLI arguments
+snaps.extend(sys.argv[1:])
+snaps = [s for s in snaps if os.path.isfile(x)]
 
-#----------------------------------------------------------
-#1. Make sure that caesar.drive works for a simba snap
-#----------------------------------------------------------
+for snap in snaps:
+    ds = yt.load(simba_snap)
+    obj = caesar.CAESAR(ds)
+    obj.member_search()
+    obj.save('caesar_simba.hdf5')
 
+    obj = caesar.load('caesar_simba.hdf5')
 
-ds = yt.load(simba_snap)
-obj = caesar.CAESAR(ds)
-obj.member_search()
-obj.save('caesar_simba.hdf5')
+    # This indirectly tests a fair few fields
+    print(obj.galinfo())
 
-obj2 = caesar.load('caesar_simba.hdf5')
+    # get the masses, radii and metallicities
+    masses = [gal.masses['total'] for gal in obj.galaxies]
+    metallicity = [gal.metallicity for gal in obj.galaxies]
+    radii = [gal.radii['total_half_mass'] for gal in obj.galaxies]
 
-#get the galinfo
-print(obj.galinfo())
-
-#get the masses, radii and metallicities
-masses = [gal.masses['total'] for gal in obj.galaxies]
-metallicity = [gal.metallicity for gal in obj.galaxies]
-radii = [gal.radii['total_half_mass'] for gal in obj.galaxies]
-
-
-
-#----------------------------------------------------------
-#2. Now do it all over again for a generic snapshot
-#----------------------------------------------------------
-
-ds = yt.load(small_snap)
-obj = caesar.CAESAR(ds)
-obj.member_search()
-obj.save('caesar_small.hdf5')
-
-obj2 = caesar.load('caesar_small.hdf5')
-
-#get the galinfo
-print(obj.galinfo())
-
-#get the masses, radii and metallicities
-masses = [gal.masses['total'] for gal in obj.galaxies]
-metallicity = [gal.metallicity for gal in obj.galaxies]
-radii =    [gal.radii['total_half_mass'] for gal in obj.galaxies]
